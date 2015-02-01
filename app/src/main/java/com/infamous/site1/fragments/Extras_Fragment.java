d3ab9b9d012fc68dcbe833ce5a7f84eb57a933bf
Jamison904 <james.jamison85@gmail.com>
2015-01-31 22:12:38 -0500

    Home and start extras

--- app/src/main/java/com/infamous/site1/fragments/Extras_Fragment.java
+++ app/src/main/java/com/infamous/site1/fragments/Extras_Fragment.java
@@ -1,23 +1,66 @@
 package com.infamous.site1.fragments;
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.infamous.site1.R;
+import android.content.*;
+import android.net.*;
+import android.os.*;
+import android.support.v4.app.*;
+import android.view.*;
+import android.widget.*;
+import com.infamous.site1.*;
+import com.infamous.site1.activities.*;
+import java.util.*;
 
 
 /**
  * Created by Daniel on 09.11.2014.
  */
-public class Extras_Fragment extends Fragment {
+public class Extras_Fragment extends Fragment implements View.OnClickListener {
+	
+	public static final int APPLY = 0;
+    ArrayList<String> dialogItems;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_extras, container, false);
+		
+        Button dialog = (Button) rootView.findViewById(R.id.buttonStand);
+        Button singleDialog = (Button) rootView.findViewById(R.id.buttonSingle);
+        Button persSingleDialog = (Button) rootView.findViewById(R.id.buttonPersSingle);
+        Button multiDialog = (Button) rootView.findViewById(R.id.buttonMulti);
+
+        dialog.setOnClickListener(this);
+        singleDialog.setOnClickListener(this);
+        persSingleDialog.setOnClickListener(this);
+        multiDialog.setOnClickListener(this);
+
+        dialogItems = new ArrayList<String>();
+
+        for (int i = 0; i < 10; i++) {
+            dialogItems.add("Item No. "+i);
+        }
+
         return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonStand:
+                Intent forum = new Intent(Intent.ACTION_VIEW).setData(Uri.parse
+				("http://infamousdevelopment.com/forum")); 
+				startActivity(forum);
+                break;
+            case R.id.buttonSingle:
+                MainActivity.showMySingleDialog("Dialog", dialogItems, "cancel", "ok");
+                break;
+            case R.id.buttonPersSingle:
+                Intent request = new Intent(Intent.ACTION_VIEW).setData(Uri.parse
+				("http://infamousdevelopment.com"));
+				startActivity(request);break;
+            case R.id.buttonMulti:
+                MainActivity.showMyMultiDialog("Dialog", dialogItems, "cancel", "ok");
+                break;
+        }
     }
 
 }
--- app/src/main/java/com/infamous/site1/fragments/Home_Fragment.java
+++ app/src/main/java/com/infamous/site1/fragments/Home_Fragment.java
@@ -26,11 +26,12 @@ public class Home_Fragment extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
-        Button dialog = (Button) rootView.findViewById(R.id.buttonStand);
-        Button singleDialog = (Button) rootView.findViewById(R.id.buttonSingle);
-        Button persSingleDialog = (Button) rootView.findViewById(R.id.buttonPersSingle);
-        Button multiDialog = (Button) rootView.findViewById(R.id.buttonMulti);
+        Button dialog = (Button) rootView.findViewById(R.id.site);
+        Button singleDialog = (Button) rootView.findViewById(R.id.apps);
+        Button persSingleDialog = (Button) rootView.findViewById(R.id.blog);
+        Button multiDialog = (Button) rootView.findViewById(R.id.downloads);
 
+		
         dialog.setOnClickListener(this);
         singleDialog.setOnClickListener(this);
         persSingleDialog.setOnClickListener(this);
@@ -48,20 +49,26 @@ public class Home_Fragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.buttonStand:
+            case R.id.site:
                 Intent forum = new Intent(Intent.ACTION_VIEW).setData(Uri.parse
 				("http://infamousdevelopment.com/forum")); 
 				startActivity(forum);
                 break;
-            case R.id.buttonSingle:
-                MainActivity.showMySingleDialog("Dialog", dialogItems, "cancel", "ok");
-                break;
-            case R.id.buttonPersSingle:
-                MainActivity.showMyRadioDialog("Dialog", dialogItems, "cancel", "ok");
-                break;
-            case R.id.buttonMulti:
-                MainActivity.showMyMultiDialog("Dialog", dialogItems, "cancel", "ok");
-                break;
+            case R.id.apps:
+                Intent black = new Intent(Intent.ACTION_VIEW).setData(Uri.parse
+				("http://infamousgit.com"));
+				startActivity(black);
+				break;
+            case R.id.blog:
+                Intent request = new Intent(Intent.ACTION_VIEW).setData(Uri.parse
+				("http://infamousdevelopment.com"));
+				startActivity(request);
+				break;
+            case R.id.downloads:
+                Intent downloads = new Intent(Intent.ACTION_VIEW).setData(Uri.parse
+				("http://infamous-downloads.github.io/"));
+				startActivity(downloads);
+				break;
         }
     }
 }
